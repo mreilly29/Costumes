ce59d7076288fdbc6bab81c57248cf7096f393b8
mreilly29 <mreilly18@jcu.edu>
2018-11-01 10:53:05 -0400

    Refactored JavaScript and CSS

--- main.js
+++ main.js
@@ -1,28 +1,29 @@
 console.log("hello");
+
 fetch('data.json')
     .then((res)=> res.json())
     .then((data)=> { 
         const title = document.querySelector('#category-label');
-        title.innerHTML=  data.CostumeType + ' ' + data.Year;
+        const {CostumeType,Costumes,Year}=data;
+        title.innerHTML=  `${CostumeType} ${Year}`;
 
-        const section = document.querySelector('#costumes');
+        const section = document.querySelector('#costumes');        
         
-        
-        for (const costume of data.Costumes) {
+        for (const costume of Costumes) {
+            const {Name, ImageUrl, IsMovieRelated} = costume
             const article = document.createElement('article');
             const h3 = document.createElement('h3');
             const p = document.createElement('p');
+
             const image = document.createElement('img');
-            image.src=costume.ImageUrl;
+            image.src=ImageUrl;
+
             section.appendChild(article);
             article.appendChild(image);
-            article.appendChild(h3).innerHTML = costume.Name;
-            article.appendChild(p).innerHTML = costume.IsMovieRelated ? 'Is Movie Related' : 'Is NOT Movie Related';;
 
-            
-        
-
-            console.log(costume.Name);
-            console.log(costume.IsMovieRelated);
+            article.appendChild(h3).innerHTML = Name;
+            article.appendChild(p).innerHTML = IsMovieRelated ? 
+                'Is Movie Related' : 
+                'Is NOT Movie Related';
         }
     });
